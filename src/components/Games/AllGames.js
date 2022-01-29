704a2d98f32fa60d293be5168b8bbe9f31990c81
douglas-devinhouse <douglas_nascimento4@estudante.sesisenai.org.br>
2022-01-29 15:13:10 -0300

    Refatorações

--- src/components/Games/AllGames.js
+++ src/components/Games/AllGames.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default ({title, items}) => {
+const AllGames = ({title, items}) => {
     return (
         <div>
-            <h2>{title}</h2>            
-            
+            <h2>{title}</h2>                        
+
             {items.length > 0 && items.map((item, key) => (
                 <img src={item.thumbnail} alt={item.title} key={key}></img>
             ))}
@@ -12,3 +12,5 @@ export default ({title, items}) => {
         </div>
     );
 }
+
+export default AllGames;
